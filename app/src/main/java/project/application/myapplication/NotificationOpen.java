2de6e46d877f74dc76ce055f1fa61fd53c2860af
Bogdan <bogdankryveiko@gmail.com>
2023-04-14 22:51:59 +0300

    Daily Notification Miwa,Rlazurk,Humanity_restored,ihar_b wvim papa

---
+++ app/src/main/java/project/application/myapplication/NotificationOpen.java
@@ -0,0 +1,16 @@
+package project.application.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationOpen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_open);
+
+
+    }
+}
