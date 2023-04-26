678c18925adfead444794d17d7602dc62e782409
ihar-b <romanigor007@gmail.com>
2023-04-26 20:09:10 +0300

    adding design

--- app/src/main/java/project/application/myapplication/MainActivity.java
+++ app/src/main/java/project/application/myapplication/MainActivity.java
@@ -1,24 +1,15 @@
 package project.application.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
-import android.app.AlarmManager;
-import android.app.AlertDialog;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Calendar;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,14 +23,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView currentBudget;
     private SpendingsDBOpenHelper mDB;
 
-    @SuppressLint("MissingInflatedId")
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        mDB = new SpendingsDBOpenHelper(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mDB = new SpendingsDBOpenHelper(this);
-
         goToMain = findViewById(R.id.goToMain);
         goToActivity_2 = findViewById(R.id.goToActivity_2);
         goToActivity_3 = findViewById(R.id.goToActivity_3);
@@ -47,10 +36,8 @@ public class MainActivity extends AppCompatActivity {
         addBudget = findViewById(R.id.addBudget);
         enterBudgetOrExpenses = findViewById(R.id.enterBudgetOrExpenses);
         currentBudget = findViewById(R.id.currentBudget);
-
         budget = mDB.query(7);
         currentBudget.setText(budget.toString());
-
         addExpenses.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
         addBudget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
         goToActivity_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(click);
             }
         });
-
         goToActivity_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -91,61 +75,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(click);
             }
         });
-
         goToMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
-
-        // Set up the notification channel
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Dwd";
-            String description = "TRWr";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("Notification", name, importance);
-            channel.setDescription(description);
-
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-
-        // Set up the daily alarm to send notifications
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 10);
-        calendar.set(Calendar.MINUTE, 00);
-        calendar.set(Calendar.SECOND, 00);
-
-        if (Calendar.getInstance().after(calendar)) {
-            calendar.add(Calendar.DAY_OF_MONTH, 1);
-        }
-
-        Intent intent = new Intent(MainActivity.this, MemoBroadcast.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-
-        new AlertDialog.Builder(this)
-                .setMessage("Are you sure to exit ?")
-                .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                        finish();
-
-                    }
-                })
-                .setNegativeButton("No",null)
-                .show();
     }
 }
