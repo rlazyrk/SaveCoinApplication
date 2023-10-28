c1f16575efe6d8d256bdd066e4ea485491d64e6e
Andriy723 <113835398+Andriy723@users.noreply.github.com>
2023-10-28 22:01:16 +0300

    Adding spending_page and some changes in Activity_3. Also changing in notification_page

--- app/src/main/java/project/application/myapplication/Activity_3.java
+++ app/src/main/java/project/application/myapplication/Activity_3.java
@@ -25,18 +25,31 @@ public class Activity_3 extends AppCompatActivity {
     private Integer expensesType_3 = 0;
     private Integer expensesType_4 = 0;
     private Integer expensesType_5 = 0;
+    private Integer expensesType_6 = 0;
+    private Integer expensesType_7 = 0;
+    private Integer expensesType_8 = 0;
+    private Integer expensesType_9 = 0;
 
     private Button btnType_1;
     private Button btnType_2;
     private Button btnType_3;
     private Button btnType_4;
     private Button btnType_5;
+    private Button btnType_6;
+    private Button btnType_7;
+    private Button btnType_8;
+    private Button btnType_9;
+
     private EditText editType_1;
     private TextView viewType_1;
     private TextView viewType_2;
     private TextView viewType_3;
     private TextView viewType_4;
     private TextView viewType_5;
+    private TextView viewType_6;
+    private TextView viewType_7;
+    private TextView viewType_8;
+    private TextView viewType_9;
 
     private TextView viewSumOfAll;
     private Button goToMain;
@@ -44,7 +57,6 @@ public class Activity_3 extends AppCompatActivity {
     private Button goToActivity_3;
     private SpendingsDBOpenHelper mDB;
     private Integer sumOfAllint = 0;
-
 
 
     @Override
@@ -59,12 +71,20 @@ public class Activity_3 extends AppCompatActivity {
         btnType_3 = findViewById(R.id.addType_3);
         btnType_4 = findViewById(R.id.addType_4);
         btnType_5 = findViewById(R.id.addType_5);
+        btnType_6 = findViewById(R.id.addType_6);
+        btnType_7 = findViewById(R.id.addType_7);
+        btnType_8 = findViewById(R.id.addType_8);
+        btnType_9 = findViewById(R.id.addType_9);
         editType_1 = findViewById(R.id.enterType_1);
         viewType_1 = findViewById(R.id.viewType_1);
         viewType_2 = findViewById(R.id.viewType_2);
         viewType_3 = findViewById(R.id.viewType_3);
         viewType_4 = findViewById(R.id.viewType_4);
         viewType_5 = findViewById(R.id.viewType_5);
+        viewType_6 = findViewById(R.id.viewType_6);
+        viewType_7 = findViewById(R.id.viewType_7);
+        viewType_8 = findViewById(R.id.viewType_8);
+        viewType_9 = findViewById(R.id.viewType_9);
         viewSumOfAll = findViewById(R.id.viewSumOfAll);
         mDB = new SpendingsDBOpenHelper(this);
         viewType_1.setText(expensesType_1.toString());
@@ -72,21 +92,29 @@ public class Activity_3 extends AppCompatActivity {
         viewType_3.setText(expensesType_3.toString());
         viewType_4.setText(expensesType_4.toString());
         viewType_5.setText(expensesType_5.toString());
-        sumOfAllint = mDB.querySum(5);
+        viewType_6.setText(expensesType_6.toString());
+        viewType_7.setText(expensesType_7.toString());
+        viewType_8.setText(expensesType_8.toString());
+        viewType_9.setText(expensesType_9.toString());
+        sumOfAllint = mDB.querySum(9);
         expensesType_1 = mDB.query(1);
         expensesType_2 = mDB.query(2);
         expensesType_3 = mDB.query(3);
         expensesType_4 = mDB.query(4);
         expensesType_5 = mDB.query(5);
+        expensesType_6 = mDB.query(6);
+        expensesType_7 = mDB.query(7);
+        expensesType_8 = mDB.query(8);
+        expensesType_9 = mDB.query(9);
         viewType_1.setText(expensesType_1.toString());
         viewType_2.setText(expensesType_2.toString());
         viewType_3.setText(expensesType_3.toString());
         viewType_4.setText(expensesType_4.toString());
         viewType_5.setText(expensesType_5.toString());
-
-
-
-
+        viewType_6.setText(expensesType_6.toString());
+        viewType_7.setText(expensesType_7.toString());
+        viewType_8.setText(expensesType_8.toString());
+        viewType_9.setText(expensesType_9.toString());
 
         viewSumOfAll.setText(sumOfAllint.toString());
         btnType_1.setOnClickListener(new View.OnClickListener() {
@@ -98,7 +126,7 @@ public class Activity_3 extends AppCompatActivity {
                     mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 1);
                     expensesType_1 = mDB.query(1);
                     viewType_1.setText(expensesType_1.toString());
-                    sumOfAllint = mDB.querySum(5);
+                    sumOfAllint = mDB.querySum(9);
                     viewSumOfAll.setText(sumOfAllint.toString());
                 }
             }
@@ -112,7 +140,7 @@ public class Activity_3 extends AppCompatActivity {
                     mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 2);
                     expensesType_2 = mDB.query(2);
                     viewType_2.setText(expensesType_2.toString());
-                    sumOfAllint = mDB.querySum(5);
+                    sumOfAllint = mDB.querySum(9);
                     viewSumOfAll.setText(sumOfAllint.toString());
                 }
             }
@@ -126,7 +154,7 @@ public class Activity_3 extends AppCompatActivity {
                     mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 3);
                     expensesType_3 = mDB.query(3);
                     viewType_3.setText(expensesType_3.toString());
-                    sumOfAllint = mDB.querySum(5);
+                    sumOfAllint = mDB.querySum(9);
                     viewSumOfAll.setText(sumOfAllint.toString());
                 }
             }
@@ -140,7 +168,7 @@ public class Activity_3 extends AppCompatActivity {
                     mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 4);
                     expensesType_4 = mDB.query(4);
                     viewType_4.setText(expensesType_4.toString());
-                    sumOfAllint = mDB.querySum(5);
+                    sumOfAllint = mDB.querySum(9);
                     viewSumOfAll.setText(sumOfAllint.toString());
                 }
             }
@@ -155,7 +183,67 @@ public class Activity_3 extends AppCompatActivity {
                     mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 5);
                     expensesType_5 = mDB.query(5);
                     viewType_5.setText(expensesType_5.toString());
-                    sumOfAllint = mDB.querySum(5);
+                    sumOfAllint = mDB.querySum(9);
+                    viewSumOfAll.setText(sumOfAllint.toString());
+                }
+            }
+        });
+
+        btnType_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editType_1.getText().toString().trim().equals("")) {
+                    Toast.makeText(Activity_3.this, R.string.emptyField, Toast.LENGTH_LONG).show();
+                } else {
+                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 6);
+                    expensesType_6 = mDB.query(6);
+                    viewType_6.setText(expensesType_6.toString());
+                    sumOfAllint = mDB.querySum(9);
+                    viewSumOfAll.setText(sumOfAllint.toString());
+                }
+            }
+        });
+
+        btnType_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editType_1.getText().toString().trim().equals("")) {
+                    Toast.makeText(Activity_3.this, R.string.emptyField, Toast.LENGTH_LONG).show();
+                } else {
+                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 7);
+                    expensesType_7 = mDB.query(7);
+                    viewType_7.setText(expensesType_7.toString());
+                    sumOfAllint = mDB.querySum(9);
+                    viewSumOfAll.setText(sumOfAllint.toString());
+                }
+            }
+        });
+
+        btnType_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editType_1.getText().toString().trim().equals("")) {
+                    Toast.makeText(Activity_3.this, R.string.emptyField, Toast.LENGTH_LONG).show();
+                } else {
+                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 8);
+                    expensesType_8 = mDB.query(8);
+                    viewType_8.setText(expensesType_8.toString());
+                    sumOfAllint = mDB.querySum(9);
+                    viewSumOfAll.setText(sumOfAllint.toString());
+                }
+            }
+        });
+
+        btnType_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editType_1.getText().toString().trim().equals("")) {
+                    Toast.makeText(Activity_3.this, R.string.emptyField, Toast.LENGTH_LONG).show();
+                } else {
+                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 9);
+                    expensesType_9 = mDB.query(9);
+                    viewType_9.setText(expensesType_9.toString());
+                    sumOfAllint = mDB.querySum(9);
                     viewSumOfAll.setText(sumOfAllint.toString());
                 }
             }
