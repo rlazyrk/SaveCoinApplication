b393801534e7e160414785c5da7678de42d11392
MIWAMIWAMIWA <107461633+MIWAMIWAMIWA@users.noreply.github.com>
2023-10-28 22:13:59 +0300

    Revert "adding swipe and actyvity permutation"

--- app/src/main/java/project/application/myapplication/Activity_3.java
+++ app/src/main/java/project/application/myapplication/Activity_3.java
@@ -3,24 +3,48 @@ package project.application.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.DashPathEffect;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.util.Arrays;
+
 public class Activity_3 extends AppCompatActivity {
 
-    float x1, x2, y1, y2;
+    private Integer expensesType_1 = 0;
+    private Integer expensesType_2 = 0;
+    private Integer expensesType_3 = 0;
+    private Integer expensesType_4 = 0;
+    private Integer expensesType_5 = 0;
 
+    private Button btnType_1;
+    private Button btnType_2;
+    private Button btnType_3;
+    private Button btnType_4;
+    private Button btnType_5;
+    private EditText editType_1;
+    private TextView viewType_1;
+    private TextView viewType_2;
+    private TextView viewType_3;
+    private TextView viewType_4;
+    private TextView viewType_5;
 
-
-
+    private TextView viewSumOfAll;
     private Button goToMain;
     private Button goToActivity_2;
     private Button goToActivity_3;
+    private SpendingsDBOpenHelper mDB;
+    private Integer sumOfAllint = 0;
+
 
 
     @Override
@@ -30,6 +54,112 @@ public class Activity_3 extends AppCompatActivity {
         goToMain = findViewById(R.id.goToMain);
         goToActivity_2 = findViewById(R.id.goToActivity_2);
         goToActivity_3 = findViewById(R.id.goToActivity_3);
+        btnType_1 = findViewById(R.id.addType_1);
+        btnType_2 = findViewById(R.id.addType_2);
+        btnType_3 = findViewById(R.id.addType_3);
+        btnType_4 = findViewById(R.id.addType_4);
+        btnType_5 = findViewById(R.id.addType_5);
+        editType_1 = findViewById(R.id.enterType_1);
+        viewType_1 = findViewById(R.id.viewType_1);
+        viewType_2 = findViewById(R.id.viewType_2);
+        viewType_3 = findViewById(R.id.viewType_3);
+        viewType_4 = findViewById(R.id.viewType_4);
+        viewType_5 = findViewById(R.id.viewType_5);
+        viewSumOfAll = findViewById(R.id.viewSumOfAll);
+        mDB = new SpendingsDBOpenHelper(this);
+        viewType_1.setText(expensesType_1.toString());
+        viewType_2.setText(expensesType_2.toString());
+        viewType_3.setText(expensesType_3.toString());
+        viewType_4.setText(expensesType_4.toString());
+        viewType_5.setText(expensesType_5.toString());
+        sumOfAllint = mDB.querySum(5);
+        expensesType_1 = mDB.query(1);
+        expensesType_2 = mDB.query(2);
+        expensesType_3 = mDB.query(3);
+        expensesType_4 = mDB.query(4);
+        expensesType_5 = mDB.query(5);
+        viewType_1.setText(expensesType_1.toString());
+        viewType_2.setText(expensesType_2.toString());
+        viewType_3.setText(expensesType_3.toString());
+        viewType_4.setText(expensesType_4.toString());
+        viewType_5.setText(expensesType_5.toString());
+
+
+
+
+
+        viewSumOfAll.setText(sumOfAllint.toString());
+        btnType_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editType_1.getText().toString().trim().equals("")) {
+                    Toast.makeText(Activity_3.this, R.string.emptyField, Toast.LENGTH_LONG).show();
+                } else {
+                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 1);
+                    expensesType_1 = mDB.query(1);
+                    viewType_1.setText(expensesType_1.toString());
+                    sumOfAllint = mDB.querySum(5);
+                    viewSumOfAll.setText(sumOfAllint.toString());
+                }
+            }
+        });
+        btnType_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editType_1.getText().toString().trim().equals("")) {
+                    Toast.makeText(Activity_3.this, R.string.emptyField, Toast.LENGTH_LONG).show();
+                } else {
+                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 2);
+                    expensesType_2 = mDB.query(2);
+                    viewType_2.setText(expensesType_2.toString());
+                    sumOfAllint = mDB.querySum(5);
+                    viewSumOfAll.setText(sumOfAllint.toString());
+                }
+            }
+        });
+        btnType_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editType_1.getText().toString().trim().equals("")) {
+                    Toast.makeText(Activity_3.this, R.string.emptyField, Toast.LENGTH_LONG).show();
+                } else {
+                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 3);
+                    expensesType_3 = mDB.query(3);
+                    viewType_3.setText(expensesType_3.toString());
+                    sumOfAllint = mDB.querySum(5);
+                    viewSumOfAll.setText(sumOfAllint.toString());
+                }
+            }
+        });
+        btnType_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editType_1.getText().toString().trim().equals("")) {
+                    Toast.makeText(Activity_3.this, R.string.emptyField, Toast.LENGTH_LONG).show();
+                } else {
+                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 4);
+                    expensesType_4 = mDB.query(4);
+                    viewType_4.setText(expensesType_4.toString());
+                    sumOfAllint = mDB.querySum(5);
+                    viewSumOfAll.setText(sumOfAllint.toString());
+                }
+            }
+        });
+
+        btnType_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editType_1.getText().toString().trim().equals("")) {
+                    Toast.makeText(Activity_3.this, R.string.emptyField, Toast.LENGTH_LONG).show();
+                } else {
+                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 5);
+                    expensesType_5 = mDB.query(5);
+                    viewType_5.setText(expensesType_5.toString());
+                    sumOfAllint = mDB.querySum(5);
+                    viewSumOfAll.setText(sumOfAllint.toString());
+                }
+            }
+        });
 
         goToMain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,26 +182,5 @@ public class Activity_3 extends AppCompatActivity {
                 startActivity(click);
             }
         });
-    }
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                x1 = event.getX();
-                y1 = event.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                x2 = event.getX();
-                y2 = event.getY();
-                if(x1 < x2){
-                    Intent i = new Intent(Activity_3.this, MainActivity.class);
-                    startActivity(i);
-                    overridePendingTransition(R.xml.slide_right_start, R.xml.slide_right_end);
-                }
-                else if (x1 > x2){
-                    return false;
-                }
-        }
-        return false;
     }
 }
--- app/src/main/java/project/application/myapplication/MainActivity.java
+++ app/src/main/java/project/application/myapplication/MainActivity.java
@@ -1,11 +1,15 @@
 package project.application.myapplication;
 
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,148 +17,78 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    float x1, x2, y1, y2;
-    private Integer expensesType_1 = 0;
-    private Integer expensesType_2 = 0;
-    private Integer expensesType_3 = 0;
-    private Integer expensesType_4 = 0;
-    private Integer expensesType_5 = 0;
-
-    private Button btnType_1;
-    private Button btnType_2;
-    private Button btnType_3;
-    private Button btnType_4;
-    private Button btnType_5;
-    private EditText editType_1;
-    private TextView viewType_1;
-    private TextView viewType_2;
-    private TextView viewType_3;
-    private TextView viewType_4;
-    private TextView viewType_5;
-
-    private TextView viewSumOfAll;
     private Button goToMain;
     private Button goToActivity_2;
     private Button goToActivity_3;
+    private Button addExpenses;
+    private Button addBudget;
+    private EditText enterBudgetOrExpenses;
+    private Integer budget = 0;
+    private TextView currentBudget;
     private SpendingsDBOpenHelper mDB;
-    private Integer sumOfAllint = 0;
-
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDB = new SpendingsDBOpenHelper(this);
+
         goToMain = findViewById(R.id.goToMain);
         goToActivity_2 = findViewById(R.id.goToActivity_2);
         goToActivity_3 = findViewById(R.id.goToActivity_3);
-        btnType_1 = findViewById(R.id.addType_1);
-        btnType_2 = findViewById(R.id.addType_2);
-        btnType_3 = findViewById(R.id.addType_3);
-        btnType_4 = findViewById(R.id.addType_4);
-        btnType_5 = findViewById(R.id.addType_5);
-        editType_1 = findViewById(R.id.enterType_1);
-        viewType_1 = findViewById(R.id.viewType_1);
-        viewType_2 = findViewById(R.id.viewType_2);
-        viewType_3 = findViewById(R.id.viewType_3);
-        viewType_4 = findViewById(R.id.viewType_4);
-        viewType_5 = findViewById(R.id.viewType_5);
-        viewSumOfAll = findViewById(R.id.viewSumOfAll);
-        mDB = new SpendingsDBOpenHelper(this);
-        viewType_1.setText(expensesType_1.toString());
-        viewType_2.setText(expensesType_2.toString());
-        viewType_3.setText(expensesType_3.toString());
-        viewType_4.setText(expensesType_4.toString());
-        viewType_5.setText(expensesType_5.toString());
-        sumOfAllint = mDB.querySum(5);
-        expensesType_1 = mDB.query(1);
-        expensesType_2 = mDB.query(2);
-        expensesType_3 = mDB.query(3);
-        expensesType_4 = mDB.query(4);
-        expensesType_5 = mDB.query(5);
-        viewType_1.setText(expensesType_1.toString());
-        viewType_2.setText(expensesType_2.toString());
-        viewType_3.setText(expensesType_3.toString());
-        viewType_4.setText(expensesType_4.toString());
-        viewType_5.setText(expensesType_5.toString());
+        addExpenses = findViewById(R.id.addExpenses);
+        addBudget = findViewById(R.id.addBudget);
+        enterBudgetOrExpenses = findViewById(R.id.enterBudgetOrExpenses);
+        currentBudget = findViewById(R.id.currentBudget);
 
+        budget = mDB.query(7);
+        currentBudget.setText(budget.toString());
 
-
-
-
-        viewSumOfAll.setText(sumOfAllint.toString());
-        btnType_1.setOnClickListener(new View.OnClickListener() {
+        addExpenses.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (editType_1.getText().toString().trim().equals("")) {
+                if (enterBudgetOrExpenses.getText().toString().trim().equals("")) {
                     Toast.makeText(MainActivity.this, R.string.emptyField, Toast.LENGTH_LONG).show();
                 } else {
-                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 1);
-                    expensesType_1 = mDB.query(1);
-                    viewType_1.setText(expensesType_1.toString());
-                    sumOfAllint = mDB.querySum(5);
-                    viewSumOfAll.setText(sumOfAllint.toString());
-                }
-            }
-        });
-        btnType_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (editType_1.getText().toString().trim().equals("")) {
-                    Toast.makeText(MainActivity.this, R.string.emptyField, Toast.LENGTH_LONG).show();
-                } else {
-                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 2);
-                    expensesType_2 = mDB.query(2);
-                    viewType_2.setText(expensesType_2.toString());
-                    sumOfAllint = mDB.querySum(5);
-                    viewSumOfAll.setText(sumOfAllint.toString());
-                }
-            }
-        });
-        btnType_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (editType_1.getText().toString().trim().equals("")) {
-                    Toast.makeText(MainActivity.this, R.string.emptyField, Toast.LENGTH_LONG).show();
-                } else {
-                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 3);
-                    expensesType_3 = mDB.query(3);
-                    viewType_3.setText(expensesType_3.toString());
-                    sumOfAllint = mDB.querySum(5);
-                    viewSumOfAll.setText(sumOfAllint.toString());
-                }
-            }
-        });
-        btnType_4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (editType_1.getText().toString().trim().equals("")) {
-                    Toast.makeText(MainActivity.this, R.string.emptyField, Toast.LENGTH_LONG).show();
-                } else {
-                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 4);
-                    expensesType_4 = mDB.query(4);
-                    viewType_4.setText(expensesType_4.toString());
-                    sumOfAllint = mDB.querySum(5);
-                    viewSumOfAll.setText(sumOfAllint.toString());
+                    budget -= Integer.parseInt(enterBudgetOrExpenses.getText().toString().trim());
+                    currentBudget.setText(budget.toString());
                 }
             }
         });
 
-        btnType_5.setOnClickListener(new View.OnClickListener() {
+        addBudget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (editType_1.getText().toString().trim().equals("")) {
+                if (enterBudgetOrExpenses.getText().toString().trim().equals("")) {
                     Toast.makeText(MainActivity.this, R.string.emptyField, Toast.LENGTH_LONG).show();
                 } else {
-                    mDB.insert(Integer.parseInt(editType_1.getText().toString().trim()), 5);
-                    expensesType_5 = mDB.query(5);
-                    viewType_5.setText(expensesType_5.toString());
-                    sumOfAllint = mDB.querySum(5);
-                    viewSumOfAll.setText(sumOfAllint.toString());
+                    mDB.insert(Integer.parseInt(enterBudgetOrExpenses.getText().toString().trim()), 7);
+                    budget = mDB.query(7);
+                    currentBudget.setText(budget.toString());
                 }
+            }
+        });
+
+        goToActivity_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent click = new Intent(MainActivity.this, Activity_2.class);
+                startActivity(click);
+            }
+        });
+
+        goToActivity_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent click = new Intent(MainActivity.this, Activity_3.class);
+                startActivity(click);
             }
         });
 
@@ -164,45 +98,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        goToActivity_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent click = new Intent(MainActivity.this, Activity_3.class);
-                startActivity(click);
-            }
 
-        });
-        goToActivity_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent click = new Intent(MainActivity.this, Activity_2.class);
-                startActivity(click);
-            }
-        });
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                x1 = event.getX();
-                y1 = event.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                x2 = event.getX();
-                y2 = event.getY();
-                if(x1 < x2){
-                    Intent i = new Intent(MainActivity.this, Activity_2.class);
-                    startActivity(i);
-                    overridePendingTransition(R.xml.slide_right_start, R.xml.slide_right_end);
-                }
-                else if (x1 > x2){
 
-                    Intent i = new Intent(MainActivity.this, Activity_3.class);
-                    startActivity(i);
-                    overridePendingTransition(R.xml.slide_left_start, R.xml.slide_left_end);
-                }
-        }
-        return false;
+    @Override
+    public void onBackPressed() {
+
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure to exit ?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        finish();
+
+                    }
+                })
+                .setNegativeButton("No",null)
+                .show();
     }
 }
