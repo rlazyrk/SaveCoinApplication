833a5aee4c2aa124ae74ee8e357104598cfcbf57
bogdan9876 <bogdankryveiko@gmail.com>
2023-10-29 15:28:35 +0200

    add demo notification button

--- app/src/main/java/project/application/myapplication/MainActivity.java
+++ app/src/main/java/project/application/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import java.util.Calendar;
 
@@ -63,6 +64,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button notificationButton = findViewById(R.id.notificationButton);
+        notificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel("default", "Default", NotificationManager.IMPORTANCE_DEFAULT);
+                    notificationManager.createNotificationChannel(channel);
+                }
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "default")
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                notificationManager.notify(0, builder.build());
+            }
+        });
+
+
+
         addBudget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -101,7 +122,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     @Override
     public void onBackPressed() {
 
@@ -120,3 +140,4 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 }
+
